104ebe252ace0dfb6504f648104934ad0e72d02e
JustOnesAndZeros <benwray2005@gmail.com>
2022-05-21 09:28:10 +0100

    commented code

--- Assets/Scripts/KillOnContact.cs
+++ Assets/Scripts/KillOnContact.cs
@@ -10,6 +10,6 @@ public class KillOnContact : MonoBehaviour
     
     private void OnCollisionEnter2D(Collision2D col)
     {
-        OnDeath?.Invoke(col.gameObject);
+        OnDeath?.Invoke(col.gameObject); //triggers player death event
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -15,12 +15,18 @@ public class PlayerController : MonoBehaviour
     [Space]
     [SerializeField] private float moveSpeed;
     [SerializeField] private float jumpForce;
-    
-    private void OnEnable()
+    private float _moveDirection;
+
+    //get rigidbody and collider
+    private void Awake()
     {
         _rb = GetComponent<Rigidbody2D>();
         _col = GetComponent<Collider2D>();
-        
+    }
+
+    //enable user input and subscribe to events
+    private void OnEnable()
+    {
         _playerInputActions = new PlayerInputActions();
         _playerInputActions.Enable();
 
@@ -31,6 +37,7 @@ public class PlayerController : MonoBehaviour
         KillOnContact.OnDeath += OnDeath;
     }
 
+    //unsubscribes from events
     private void OnDisable()
     {
         _playerInputActions.Movement.Jump.started -= OnJump;
@@ -42,26 +49,32 @@ public class PlayerController : MonoBehaviour
     
     private void FixedUpdate()
     {
-        float moveDirection = _horizontalMove.ReadValue<float>();
-        if (!CheckDirection(new Vector2(moveDirection, 0)))
+        if (Math.Abs(_moveDirection - _horizontalMove.ReadValue<float>()) > 0) return; //will not change direction if input hasn't changed
+        
+        //sets horizontal player movement
+        _moveDirection = _horizontalMove.ReadValue<float>();
+        if (!CheckDirection(new Vector2(_moveDirection, 0))) //will not move player if a wall is in that direction (prevents sticking to walls)
         {
-            _rb.velocity = new Vector2(_horizontalMove.ReadValue<float>() * moveSpeed, _rb.velocity.y);
+            _rb.velocity = new Vector2(_horizontalMove.ReadValue<float>() * moveSpeed, _rb.velocity.y); //moves player 
         }
     }
 
     private void OnJump(InputAction.CallbackContext ctx)
     {
-        if (CheckDirection(Vector2.down)) _rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        if (CheckDirection(Vector2.down)) _rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse); //applies vertical force to player
     }
 
+    //checks if the player is touching a wall in the specified direction
+    //used for ground checks and to prevent sticking to walls
     private bool CheckDirection(Vector2 direction)
     {
         var bounds = _col.bounds;
         return Physics2D.BoxCast(bounds.center, bounds.size, 0f, direction, .1f, environment);
     }
 
+    //executed when the player collides with a lethal object
     private void OnDeath(GameObject player)
     {
-        transform.position = spawn.transform.position;
+        transform.position = spawn.transform.position; //teleports player back to spawn point
     }
 }
